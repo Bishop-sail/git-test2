97a28a92d4758b531e226543d2df81a77d0722dd
Bishop-sail <zyxi0222@163.com>
2021-09-11 21:16:47 +0800

    first commit

---
+++ src/main/java/com/athome/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.athome.git;
+
+/**
+ * @author Bishop
+ * @create 2021-09-11 20:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
